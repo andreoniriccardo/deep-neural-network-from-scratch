91a707aa052c1968a6fff0fde02989b4c0e77a57
Riccardo Andreoni <randreoni@kdc-one.com>
2022-09-06 16:02:21 +0200

    created repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Deep Neural Network from Scratch
+In this project, I build a deep neural network without the aid of any deep learning library (Tensorflow, Keras, Pytorch, …).
+The reason for imposing myself on this task is that nowadays it is effortless to build deep and complex neural networks using the high-level tools provided by some python libraries. This approach allows machine learning practitioners to create powerful models with just a few lines of code, but it has the massive downside of leaving the functioning of those networks unclear.
--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-q=1
+import numpy as np
+import pandas as pd
+
+def shuffle_data(data):
+  """
+  This function shuffles the row of the input dataframe.
+  Input: data
+  Output: data (shuffled)
+  """
+  # Convert input dataframe to ndarray
+  data = np.array(data)
+  return np.random.shuffle(data)
+  
+def main():
+  df_train = pd.read_csv('train.csv')
+
+  # shuffle the data
+  df_train = shuffle_data(df_train)
+
+  # split train and validation set
+  train_val_split = 0.8
+  train_size = round(df_train.shape[0] * train_val_split)
+  #X_train = df_train[]
+
+
+
+
+if __name__ == '__main__':
+  main()
